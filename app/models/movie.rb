57ee53a16bba5c07fd7474de3835a0026ab92627
jroyals2 <jamesroyals3@gmail.com>
2017-10-30 16:04:02 -0400

    seeded the database

---
+++ app/models/movie.rb
@@ -0,0 +1,23 @@
+class Movie < ApplicationRecord
+    include HTTParty
+    base_uri 'http://www.omdbapi.com/?apikey=d31f1a94&i='
+
+    def self.generate(api_id)
+        response = get api_id
+        create!(title: response["Title"],
+                genre: response["Genre"],
+                year: response["Year"],
+                synopis: response["Plot"],
+                picture: response["Poster"]
+        )
+    end
+
+end
+
+    # Tabled code        
+        # 5.times do
+        # id = (2900000..3000000).to_a
+        # random_number = id.sample
+        # response = get("#{random_number}")
+        # puts random_number
+        # puts response
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+Movie.generate('tt0117998')
+Movie.generate('tt0117571')
+Movie.generate('tt0067800')
+Movie.generate('tt0050032')
+Movie.generate('tt0113855')
+Movie.generate('tt0117500')
+Movie.generate('tt0286106')
+Movie.generate('tt0169547')
+Movie.generate('tt0120338')
+Movie.generate('tt1314655')
