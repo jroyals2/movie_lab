ca07ba1b1e07e01e58cced754ac9d55f00977189
jroyals2 <jamesroyals3@gmail.com>
2017-10-30 16:21:06 -0400

    Did the show and index routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/new'
-
-  get 'movies/edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :movies
+  resources :movies
 end
